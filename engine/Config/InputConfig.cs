208e0cc12f34503a5fa9f3f2046be891dd36624c
tree michael <jonathon.lindemann@gmail.com>
2014-03-14 10:37:04 -0400

    merge

---
+++ engine/Config/InputConfig.cs
@@ -0,0 +1,32 @@
+﻿using OpenTK.Input;
+
+namespace engine.Config
+{
+    public static class InputConfig
+    {
+        public static float MouseSensitivity = 0.001f;
+        public const Key MoveForward = Key.W;
+        public const Key MoveBackward = Key.S;
+        public const Key MoveLeft = Key.A;
+        public const Key MoveRight = Key.D;
+        public const Key MoveUp = Key.LShift;
+        public const Key MoveDown = Key.LControl;
+        public const Key RollLeft = Key.Q;
+        public const Key RollRight = Key.E;
+        public const Key MoveFaster = Key.Plus;
+        public const Key MoveSlower = Key.Minus;
+    }
+
+    public static class MyMath
+    {
+        public static float TwoPi = 6.28f;
+        public static float ThreePiOverTwo = 4.17f;
+        public static float Pi = 3.14f;
+        public static float PiOverTwo = 1.57f;
+    }
+
+    public static class FragShader
+    {
+        public const string Mvp = "NVP";
+    }
+}
